0bea0511c0e57becc1f820153a24204864ae6e31
jungwoogyun <jwg8910@naver.com>
2023-12-27 16:39:45 +0900

    .

--- 04/src/main/java/com/example/demo/domain/dto/UserDto.java
+++ 04/src/main/java/com/example/demo/domain/dto/UserDto.java
@@ -2,14 +2,10 @@ package com.example.demo.domain.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -19,7 +15,6 @@ public class UserDto {
     @NotBlank(message="username을 입력하세요")
     private String username;
     @NotBlank(message="password 입력하세요")
-    @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",message="하나 이상의 대문자,소문자,숫자 및 특수 문자를 입력하세요." )
     private String password;
 
     @NotBlank(message="repassword 입력하세요")
@@ -40,8 +35,4 @@ public class UserDto {
     //OAUTH2
     private String provider;
     private String providerId;
-
-//    //날짜
-//    @DateTimeFormat(pattern = "yyyy-MM-dd")
-//    private LocalDateTime rdate;
 }
